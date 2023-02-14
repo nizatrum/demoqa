0bb6c391e35bc8a8e08b506a9f89ecfb2bbc3736
nizatrum <timur.murtazin434@gmail.com>
2023-02-14 09:03:26 +0400

    I keep doing my homework 2

--- src/test/java/homeWorksTests/AutomationPracticeFormTest.java
+++ src/test/java/homeWorksTests/AutomationPracticeFormTest.java
@@ -1,34 +1,58 @@
 package homeWorksTests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.crypto.Data;
-
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class automationPracticeFormTest extends DataForAutomationPracticeForm {
+public class AutomationPracticeFormTest extends DataForAutomationPracticeForm {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = BASE_URL;
         Configuration.browserSize = WINDOW_SIZE;
     }
     @Test
+    @DisplayName("Заполняем форму данными из DataForAutomationPracticeForm")
     void fillFormTest() {
-        open("/automation-practice-form");
-        $("[placeholder='First Name']").setValue(firstName);
-        $("[placeholder='Last Name']").setValue(lastName);
-        $("[placeholder='name@example.com']").setValue(email);
-        $(String.format("div.custom-radio input[value='%s'] +label", gender)).click();
-        $("[placeholder='Mobile Number']").setValue(phoneNumber);
-        $("[placeholder=First Name]").setValue(firstName);
-        actions().click($("#dateOfBirthInput")).
-                click($("select[class*='month']")).
-                click($("select[class*='month']")). // как выбрать ебучий текст по css хуяторам!"?"?"?"?""?!!!
-                click($("select[class*='month']")).
-                build().perform();
+        open("/automation-practice-form"); //открываем
+        $("#firstName").setValue(firstName); //заполняем имя
+        $("#lastName").setValue(lastName); //заполняем фамилию
+        $("#userEmail").setValue(email); //заполняем почту
+        $(String.format("div.custom-radio input[value='%s'] +label", gender)).click(); //заполняем пол
+        $("#userNumber").setValue(phoneNumber); //заполняем номер телефона
+        fillBirthDate(birthDay, birthMonth, birthYear); //заполняем дату рождения
+        $("#subjectsInput").setValue(subjects); //заполняем какие-то ярлыки :D
+        fillHobbies(hobbies); //заполняем увлечения
+        $("#uploadPicture").uploadFile(picture); // грузим фотку
+        $("#currentAddress").setValue(currentAddress); //заполняем фактический адрес
+        $("#state").scrollTo().click();
+        $(byText(state)).click();
+        $("#city").scrollTo().click();
+        $(byText(city)).click();
+        $("#submit").scrollTo().click();// ???????
 
 
+    }
+    private void fillBirthDate(byte day, String month, short year) {
+        $("#dateOfBirthInput").click();
+        $("select[class*='month']").click();
+        $(byText(month)).click(); //а если я не найду, мне надо уточнить из какого родительского элемента мы ищем внутрь, либо кто родитель.
+        $("select[class*='year']").click();
+        $(String.format("option[value='%s']", year)).click();
+        $(String.format("div[class*='day--0%s']", day)).click();
+    }
+    private void fillHobbies(String[] hobbies) {
+        for (String hobby : hobbies) {
+            $(byText(hobby)).click();
+        }
+    }
+    @AfterAll
+    static void afterAll() {
+        Selenide.closeWebDriver();
     }
 }
--- src/test/java/homeWorksTests/DataForAutomationPracticeForm.java
+++ src/test/java/homeWorksTests/DataForAutomationPracticeForm.java
@@ -10,12 +10,13 @@ public abstract class DataForAutomationPracticeForm {
     protected String email = "mytestmail@company.com";
     protected String gender = "Male";
     protected String phoneNumber = "1234567891"; //max - 10 symbol
-    protected String[] birthDate = new String[]{"21", "march", "1995"};
+    protected byte birthDay = 21;
+    protected String birthMonth = "March";
+    protected Short birthYear = 1995;
 
     protected String subjects = "Algorithms";
-    protected String[] hobbies = new String[] {"sports", "music"};
+    protected String[] hobbies = new String[] {"Sports", "Music"};
     protected File picture = new File("src/test/resources/neo.jpg");
-    //   $("#cvFileUpload").uploadFile(cv);
     protected String currentAddress = "some address";
     protected String state = "NCR";
     protected String city = "Delhi";
