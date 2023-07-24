80408a9e9a33a3fa266ed3e84eb273a50c55ddef
Timur <timur.murtazin434@gmail.com>
2023-07-25 00:30:58 +0400

    added attachments

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,6 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
@@ -14,13 +17,14 @@ public class BaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "firefox";
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "http://10.155.56.61:4444/wd/hub/";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
-        Configuration.remote = "http://10.155.56.61:4444/wd/hub/";
         Configuration.browserCapabilities = capabilities;
+
     }
 
     @BeforeEach
@@ -34,8 +38,11 @@ public class BaseTest {
         Selenide.closeWebDriver();
     }
 
-    @Test
-    void test() {
-        Assertions.assertTrue(true);
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();;
+        Attach.addVideo();
     }
 }
